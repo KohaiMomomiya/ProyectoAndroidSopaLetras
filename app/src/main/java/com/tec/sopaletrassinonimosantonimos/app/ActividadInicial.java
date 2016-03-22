e3fb6b873a565b742bc3e208c93a18e88da68331
Esteban Fonseca Montoya <kohaimomomiya@gmail.com>
2016-03-22 17:11:26 -0600

    Ahora se puede ver la imagen en el layout inicial.

--- app/src/main/java/com/tec/sopaletrassinonimosantonimos/app/ActividadInicial.java
+++ app/src/main/java/com/tec/sopaletrassinonimosantonimos/app/ActividadInicial.java
@@ -1,11 +1,10 @@
 package com.tec.sopaletrassinonimosantonimos.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class ActividadInicial extends ActionBarActivity {
+public class ActividadInicial extends AppCompatActivity {
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -14,24 +13,7 @@ public class ActividadInicial extends ActionBarActivity {
   }
   
   @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_actividad_inicial, menu);
-    return true;
-  }
-  
-  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-    
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-    
     return super.onOptionsItemSelected(item);
   }
 }
