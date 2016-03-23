f46a28635be2905506bd2763403831e2ead5d17f
Esteban Fonseca Montoya <kohaimomomiya@gmail.com>
2016-03-22 20:36:35 -0600

    Se agregaron las bases de la actividad y layout para registrar un nuevo usuario.

--- app/src/main/java/com/tec/sopaletrassinonimosantonimos/app/ActividadInicial.java
+++ app/src/main/java/com/tec/sopaletrassinonimosantonimos/app/ActividadInicial.java
@@ -1,8 +1,10 @@
 package com.tec.sopaletrassinonimosantonimos.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ActividadInicial extends AppCompatActivity {
   
@@ -15,5 +17,11 @@ public class ActividadInicial extends AppCompatActivity {
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     return super.onOptionsItemSelected(item);
+  }
+
+  /* Acción para abrir la Activity para registrar un nuevo usuario. */
+  public void eventoRegistrarNuevoUsuario(View view) {
+    Intent intent = new Intent(this, RegistrarCuenta.class);
+    startActivity(intent);
   }
 }
