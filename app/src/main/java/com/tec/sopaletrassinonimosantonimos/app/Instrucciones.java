61c3b7a2f6d91762cde41f8a6e1c75b5ae90b197
Daniel Pacheco Lascarez <daniellascarez01@gmail.com>
2016-05-17 02:55:22 -0600

    video

--- app/src/main/java/com/tec/sopaletrassinonimosantonimos/app/Instrucciones.java
+++ app/src/main/java/com/tec/sopaletrassinonimosantonimos/app/Instrucciones.java
@@ -1,7 +1,12 @@
 package com.tec.sopaletrassinonimosantonimos.app;
 
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class Instrucciones extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class Instrucciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones);
+
+        Button buttonPlayVideo2 = (Button) findViewById(R.id.button1);
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+//displays a video file
+        VideoView mVideoView2 = (VideoView) findViewById(R.id.videoView1);
+        String uriPath2 = "android.resource://com.tec.sopaletrassinonimosantonimos.app/" + R.raw.mp4;
+        Uri uri2 = Uri.parse(uriPath2);
+        mVideoView2.setVideoURI(uri2);
+        mVideoView2.requestFocus();
+        mVideoView2.start();
+
+
+        buttonPlayVideo2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VideoView mVideoView2 = (VideoView) findViewById(R.id.videoView1);
+// VideoView mVideoView = new VideoView(this);
+                String uriPath = "android.resource://com.tec.sopaletrassinonimosantonimos.app/" + R.raw.mp4;
+                Uri uri2 = Uri.parse(uriPath);
+                mVideoView2.setVideoURI(uri2);
+                mVideoView2.requestFocus();
+                mVideoView2.start();
+            }
+        });
     }
 }
